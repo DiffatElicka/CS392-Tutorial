7b06018e934733fcd823e050b43ea79de188ac89
DiffatElicka <903869629@qq.com>
2023-10-13 21:54:17 -0500

    Add a database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,66 @@
+// Import the functions you need from the SDKs you need
+import { useEffect, useState } from 'react';
+import { getDatabase, onValue, ref, update} from 'firebase/database';
+import { useCallback, useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, update } from 'firebase/database';
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCoFBVasWE0WotANtKm9mdDkROL_tuFf8",
+  authDomain: "cs392-tutorial-2b8ad.firebaseapp.com",
+  projectId: "cs392-tutorial-2b8ad",
+  storageBucket: "cs392-tutorial-2b8ad.appspot.com",
+  messagingSenderId: "456233935010",
+  appId: "1:456233935010:web:2f16f43f4436a7d14237fb",
+  measurementId: "G-1M47BC8YWT"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+// const database = getDatabase(firebase);
+
+
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useDbData = (path) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => (
+    onValue(ref(database, path), (snapshot) => {
+     setData( snapshot.val() );
+    }, (error) => {
+      setError(error);
+    })
+  ), [ path ]);
+
+  return [ data, error ];
+};
+
+const makeResult = (error) => {
+  const timestamp = Date.now();
+  const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+  return { timestamp, error, message };
+};
+
+export const useDbUpdate = (path) => {
+  const [result, setResult] = useState();
+  const updateData = useCallback((value) => {
+    update(ref(database, path), value)
+    .then(() => setResult(makeResult()))
+    .catch((error) => setResult(makeResult(error)))
+  }, [database, path]);
+
+  return [updateData, result];
+};
